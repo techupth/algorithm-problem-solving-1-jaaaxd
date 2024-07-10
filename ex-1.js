fff0e45c543cabddbf10e8bceb80f10571c79ba4
Ja <ja.techup@gmail.com>
2024-07-10 14:07:13 +0700

    feat:Algorithm: Problem Solving 1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,15 @@
+numbers = [2, 7, 11, 15];
+
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
+console.log(twoSum(numbers, 26));
