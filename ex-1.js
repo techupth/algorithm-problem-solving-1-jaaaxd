3379e928d668888c41433b94e0bec450115257df
Ja <ja.techup@gmail.com>
2024-07-10 14:11:36 +0700

    fix: j initial value

--- ex-1.js
+++ ex-1.js
@@ -1,9 +1,9 @@
-numbers = [2, 7, 11, 15];
+numbers = [2, 7, 11, 15, 30];
 
 function twoSum(numbers, target) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
   for (let i = 0; i < numbers.length - 1; i++) {
-    for (let j = 0; j < numbers.length; j++) {
+    for (let j = i + 1; j < numbers.length; j++) {
       if (numbers[i] + numbers[j] === target) {
         return [i, j];
       }
@@ -12,4 +12,4 @@ function twoSum(numbers, target) {
   return [];
 }
 
-console.log(twoSum(numbers, 26));
+console.log(twoSum(numbers, 45));
